22b3f9e98157a019fcaa3f8984eea5e8d7bc206e
harshgtamu <harsh_g@tamu.edu>
2023-02-07 22:54:55 -0600

    Implemented most toggle functionality

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+function onLoad() {
+    page_style = localStorage.getItem("page_stylesheet_name");
+    if (page_style === null)
+    {
+        page_style = "index.css";
+    }
+    document.getElementById("stylesheet-link").setAttribute("href", page_style);
+}
+
 function onStyleChange() {
     var stylesheet = document.getElementById("stylesheet-link");
     if(stylesheet.getAttribute("href") === "index.css") {
         stylesheet.setAttribute("href", "index2.css");
+        localStorage.setItem("page_stylesheet_name", "index2.css")
     } else {
         stylesheet.setAttribute("href", "index.css");
+        localStorage.setItem("page_stylesheet_name", "index.css")
     }
 }
